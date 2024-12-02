9e4dc6fe1721a8d771d100dfc966b5ca2fd1a49b
feroz khan <feroz8386@gmail.com>
2024-12-02 10:00:05 +0530

    Initial project structure

---
+++ src/app.js
@@ -0,0 +1,5 @@
+function sayHello() {
+return "Hello from GitHub Actions!";
+}
+
+module.exports  = { sayHello};
---
+++ test/tests.js
@@ -0,0 +1,4 @@
+const { sayHello} = require('../src/app');
+
+console.log('Testing sayHello function:');
+console.log(sayHello());
